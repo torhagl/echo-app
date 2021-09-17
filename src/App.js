27344660b7e91cf428710f9775e55e529d36b518
Runar Serigstad <runar.serigstad@gmail.com>
2021-09-17 10:45:09 +0200

    Improved form layout and design

--- src/App.js
+++ src/App.js
@@ -10,22 +10,24 @@ function App() {
   const [currentOrg, setOrg] = useState("917082391")
 
   const onSubmit = (event) => {
-    event.preventDefault()
-    axios.get(getUrl(currentOrg)).then(d => setData(d.data))
+    event.preventDefault();
+    axios.get(getUrl(currentOrg)).then(d => setData(d.data));
   }
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={onSubmit}>
-          <label>
-            Organisasjonsnummer: 
-          </label>
-          <input type='text' name="qwer" value={currentOrg} onChange={e => setOrg(e.target.value)} />
-          <input type="submit" value='Submit'/>
+          <div style={{ marginBottom: "5px" }}>
+            <label>
+              Stacc organisasjonssøk:
+            </label>
+          </div>
+          <input type='text' placeholder="Search..." value={currentOrg} onChange={e => setOrg(e.target.value)} />
+          <input type="submit" value='Submit' />
         </form>
         <p>{data?.navn}</p>
         <p>{data?.organisasjonsnummer}</p>
-        <p style={{fontSize: '12px'}}>{JSON.stringify(data, null, 2)}</p>
+        <p style={{ fontSize: '12px' }}>{JSON.stringify(data, null, 2)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
